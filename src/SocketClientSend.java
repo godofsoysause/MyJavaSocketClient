f8605ff45f7a49ed4938174018189fe531177358
Chencpy6 <253545375@qq.com>
2018-06-26 15:59:02 +0800

    6/26  15:28

--- src/SocketClientSend.java
+++ src/SocketClientSend.java
@@ -1,23 +1,30 @@
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
-
+import java.util.*;
 
 public class SocketClientSend implements Runnable{
 	private Thread t;
 	private String threadName;
 	private Socket server;
-	
+	private Scanner scanner;
+	private boolean send = false;
 	public SocketClientSend(Socket server) {
 		threadName = "SocketClientSend";
 		this.server = server;
+		scanner =new Scanner(System.in);
 	}
 	public void run() {
 		try {
 			if(server!=null) {
 				DataOutputStream out = new DataOutputStream(server.getOutputStream());
 				while(true) {
-		           	 out.write(buffer,0,messageLength);
+					//仅命令行版本使用  UserInput()
+					UserInput();
+					if(send) {
+			           	out.write(buffer,0,messageLength);
+			           	send = false;
+					}
 		        }
 			}       
 	      }catch (IOException e) {
@@ -33,14 +40,19 @@ public class SocketClientSend implements Runnable{
 	         t.start ();
 	      }
 	}
-	private String UserInput() {
-		return "";
+	//命令行输入
+	private void UserInput() {
+		String message = scanner.next();
+		BuildMessage(message,0);
 	}
 	
 	byte[] buffer = new byte[1024];
 	int messageLength = 0;
-	private int BuildMessage() {
+	//type什么类型的消息，0是命令行输入  可以外部调用
+	public int BuildMessage(String s,int type) {
 		//buffer
+		send = true;
 		return messageLength;
 	}
+
 }
